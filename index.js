96218672295ec87d7f8f1bb8084d1e95b1eff527
Ilya Ermolin <tomixlove@gmail.com>
2023-04-25 13:32:08 +0300

    Реализован CRUD на MongoDB и Express

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from './router.js'
+
+const PORT = 5000;
+const DB_URL = 'mongodb+srv://tomixlove:Zbaq5sctInfs0sc0@cluster0.glxew5l.mongodb.net/?retryWrites=true&w=majority'
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', router);
+// app.use('/users', userRouter);
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+        app.listen(PORT, () => console.log('SERVER STARTED ON PORT' + PORT))
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
